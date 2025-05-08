95e9e68099c4eedbd1b657b9556bf2838c59e41c
Michael berkowitz <michael.berkowitz@gmail.com>
2025-05-08 16:48:30 -0400

    less metaprogramming in Token

--- lib/loex/token.ex
+++ lib/loex/token.ex
@@ -58,32 +58,25 @@ defmodule Loex.Token do
 
   def eof, do: %__MODULE__{type: :EOF, lexeme: ""}
 
-  %{
-    "LEFT_PAREN" => "(",
-    "RIGHT_PAREN" => ")",
-    "LEFT_BRACE" => "{",
-    "RIGHT_BRACE" => "}",
-    "COMMA" => ",",
-    "DOT" => ".",
-    "MINUS" => "-",
-    "PLUS" => "+",
-    "SEMICOLON" => ";",
-    "STAR" => "*",
-    "SLASH" => "/",
-    "BANG_EQUAL" => "!=",
-    "EQUAL_EQUAL" => "==",
-    "LESS_EQUAL" => "<=",
-    "GREATER_EQUAL" => ">=",
-    "BANG" => "!",
-    "EQUAL" => "=",
-    "LESS" => "<",
-    "GREATER" => ">"
-  }
-  |> Enum.map(fn {type, lexeme} ->
-    def unquote(String.to_atom(String.downcase(type)))() do
-      %__MODULE__{type: :"#{unquote(type)}", lexeme: unquote(lexeme)}
-    end
-  end)
+  def left_paren, do: %__MODULE__{type: :LEFT_PAREN, lexeme: "("}
+  def right_paren, do: %__MODULE__{type: :RIGHT_PAREN, lexeme: ")"}
+  def left_brace, do: %__MODULE__{type: :LEFT_BRACE, lexeme: "{"}
+  def right_brace, do: %__MODULE__{type: :RIGHT_BRACE, lexeme: "}"}
+  def comma, do: %__MODULE__{type: :COMMA, lexeme: ","}
+  def dot, do: %__MODULE__{type: :DOT, lexeme: "."}
+  def minus, do: %__MODULE__{type: :MINUS, lexeme: "-"}
+  def plus, do: %__MODULE__{type: :PLUS, lexeme: "+"}
+  def semicolon, do: %__MODULE__{type: :SEMICOLON, lexeme: ";"}
+  def star, do: %__MODULE__{type: :STAR, lexeme: "*"}
+  def slash, do: %__MODULE__{type: :SLASH, lexeme: "/"}
+  def bang_equal, do: %__MODULE__{type: :BANG_EQUAL, lexeme: "!="}
+  def equal_equal, do: %__MODULE__{type: :EQUAL_EQUAL, lexeme: "=="}
+  def less_equal, do: %__MODULE__{type: :LESS_EQUAL, lexeme: "<="}
+  def greater_equal, do: %__MODULE__{type: :GREATER_EQUAL, lexeme: ">="}
+  def bang, do: %__MODULE__{type: :BANG, lexeme: "!"}
+  def equal, do: %__MODULE__{type: :EQUAL, lexeme: "="}
+  def less, do: %__MODULE__{type: :LESS, lexeme: "<"}
+  def greater, do: %__MODULE__{type: :GREATER, lexeme: ">"}
 
   def string(str) do
     %__MODULE__{type: :STRING, lexeme: str, literal: str}
