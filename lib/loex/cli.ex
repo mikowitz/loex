8b6ffb32e9da6e5b7946e4655b6ed5cf36fd54f4
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-05-12 21:12:54 -0400

    4.1: CLI framework

---
+++ lib/loex/cli.ex
@@ -0,0 +1,47 @@
+defmodule Loex.CLI do
+  def main(args) do
+    case args do
+      [] ->
+        run_repl()
+
+      [filename] ->
+        run_file(filename)
+
+      _ ->
+        IO.puts(:stderr, "Usage: loex [script]")
+        System.stop(64)
+    end
+  end
+
+  defp run_repl do
+    IO.write("> ")
+
+    case IO.read(:line) do
+      :eof ->
+        System.stop(0)
+
+      {:error, reason} ->
+        IO.puts(:stderr, "Error reading from stdin: #{reason}")
+        System.stop(65)
+
+      data ->
+        data |> String.trim() |> run()
+        run_repl()
+    end
+  end
+
+  defp run_file(filename) do
+    case File.read(filename) do
+      {:ok, contents} ->
+        run(contents)
+
+      {:error, reason} ->
+        IO.puts(:stderr, "Error reading file: #{reason}")
+        System.stop(65)
+    end
+  end
+
+  defp run(contents) do
+    IO.puts(contents)
+  end
+end
