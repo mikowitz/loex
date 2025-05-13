35ec3a3406808bdd3c5d1c4c0cbbe61632a65fb2
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-05-12 21:06:38 -0400

    mix new

---
+++ lib/loex.ex
@@ -0,0 +1,18 @@
+defmodule Loex do
+  @moduledoc """
+  Documentation for `Loex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Loex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
